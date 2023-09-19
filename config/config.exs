c439a2be75d938d987a7c8af8f3f73f5c05124cd
ktunprasert <ktunprasert@outlook.com>
2023-09-19 18:27:50 +0100

    github_issues: logging

--- config/config.exs
+++ config/config.exs
@@ -1,3 +1,8 @@
 import Config
 
 import_config "#{Mix.env()}.exs"
+
+config :logger,
+  compile_time_purge_matching: [
+    [level_lower_than: :info]
+  ]
--- lib/issues/github_issues.ex
+++ lib/issues/github_issues.ex
@@ -1,12 +1,20 @@
 defmodule Issues.GithubIssues do
+  require Logger
+
   @github_url Application.compile_env(:issues, :github_url, "https://api.github.com")
+  @user_agent [{"User-Agent", "Kris kris@kristun.dev"}]
 
   def fetch(user, project) do
+    Logger.info("Fetching issues from Github for #{user}/#{project}")
+
     HTTPoison.get(issues_url(user, project))
     |> handle_response()
   end
 
   def handle_response({_, %{status_code: code, body: body}}) do
+    Logger.info("Response code: #{code}")
+    Logger.debug(fn -> inspect(body) end)
+
     {code |> check_for_error(), body |> Poison.Parser.parse!()}
   end
 
