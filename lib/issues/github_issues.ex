66cafc0d4074020c505dcfeb8ae956158b81a089
ktunprasert <ktunprasert@outlook.com>
2023-09-19 18:01:38 +0100

    github_issues: handle_response

--- lib/issues/github_issues.ex
+++ lib/issues/github_issues.ex
@@ -3,7 +3,15 @@ defmodule Issues.GithubIssues do
 
   def fetch(user, project) do
     HTTPoison.get(issues_url(user, project))
+    |> handle_response()
   end
+
+  def handle_response({_, %{status_code: code, body: body}}) do
+    {code |> check_for_error(), body |> Poison.Parser.parse!()}
+  end
+
+  def check_for_error(200), do: :ok
+  def check_for_error(_), do: :error
 
   def issues_url(user, project) do
     "#{@github_url}/repos/#{user}/#{project}/issues"
