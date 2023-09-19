a3c77b37ae4aaa068c341e208ddeee8775ef1df9
ktunprasert <ktunprasert@outlook.com>
2023-09-19 18:15:10 +0100

    cli_test: describe group

--- test/cli_test.exs
+++ test/cli_test.exs
@@ -4,29 +4,33 @@ defmodule Issues.CLITest do
 
   import Issues.CLI
 
-  test ":help returned by option parsing with -h and --help options" do
-    assert parse_args(["-h", "anything"]) == :help
-    assert parse_args(["--help", "anything"]) == :help
+  describe "parse_args/2" do
+    test ":help returned by option parsing with -h and --help options" do
+      assert parse_args(["-h", "anything"]) == :help
+      assert parse_args(["--help", "anything"]) == :help
+    end
+
+    test "three values returned if three given" do
+      assert parse_args(["user", "project", "99"]) == {"user", "project", 99}
+    end
+
+    test "count is defaulted if two values given" do
+      assert parse_args(["user", "project"]) == {"user", "project", 4}
+    end
   end
 
-  test "three values returned if three given" do
-    assert parse_args(["user", "project", "99"]) == {"user", "project", 99}
-  end
+  describe "sort_issues/2" do
+    test "sort descending order correctly" do
+      result = sort_issues(fake_list([1, 2, 3]), order: :desc)
+      issues = for issue <- result, do: Map.get(issue, "created_at")
+      assert issues == [3, 2, 1]
+    end
 
-  test "count is defaulted if two values given" do
-    assert parse_args(["user", "project"]) == {"user", "project", 4}
-  end
-
-  test "sort descending order correctly" do
-    result = sort_issues(fake_list([1, 2, 3]), order: :desc)
-    issues = for issue <- result, do: Map.get(issue, "created_at")
-    assert issues == [3, 2, 1]
-  end
-
-  test "sort ascending order correctly" do
-    result = sort_issues(fake_list([5, 4, 3]), order: :asc)
-    issues = for issue <- result, do: Map.get(issue, "created_at")
-    assert issues == [3, 4, 5]
+    test "sort ascending order correctly" do
+      result = sort_issues(fake_list([5, 4, 3]), order: :asc)
+      issues = for issue <- result, do: Map.get(issue, "created_at")
+      assert issues == [3, 4, 5]
+    end
   end
 
   defp fake_list(values) do
