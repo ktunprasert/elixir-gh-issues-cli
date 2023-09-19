4d59b374503bdb4ca3e501b4af585b9200b19700
ktunprasert <ktunprasert@outlook.com>
2023-09-19 17:52:17 +0100

    github_issues: read from env

--- lib/issues/github_issues.ex
+++ lib/issues/github_issues.ex
@@ -1,5 +1,11 @@
 defmodule Issues.GithubIssues do
+  @github_url Application.compile_env(:issues, :github_url, "https://api.github.com")
+
   def fetch(user, project) do
-    HTTPoison.get("https://api.github.com/repos/#{user}/#{project}/issues")
+    HTTPoison.get(issues_url(user, project))
+  end
+
+  def issues_url(user, project) do
+    "#{@github_url}/repos/#{user}/#{project}/issues"
   end
 end
